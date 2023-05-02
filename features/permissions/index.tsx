51f64148cdfac1a11b61ef78ed9165f088599da2
Christopher Salguero <csalguer@alumni.stanford.edu>
2023-05-02 00:46:13 -0700

    Fix flashing of permissions check in favor of spinner upon load

--- features/permissions/index.tsx
+++ features/permissions/index.tsx
@@ -1,7 +1,8 @@
 import { Fragment, useState, useEffect, useCallback } from 'react'
 import type { ReactElement } from 'react'
-import { Alert, AlertIcon, AlertTitle, AlertDescription, Flex } from '@chakra-ui/react'
+import { Alert, AlertIcon, AlertTitle, AlertDescription, Flex, Spinner } from '@chakra-ui/react'
 import { isNull } from 'lodash'
+import { Center } from '@chakra-ui/react'
 
 interface PermissionsProviderProps {
   children: ReactElement
@@ -32,26 +33,9 @@ const PermissionsProvider = ({ children }: PermissionsProviderProps): ReactEleme
 
   if (isNull(permission)) {
     return (
-      <Flex justifyContent={'center'} alignItems={'center'} h={'100vh'} w={'100%'}>
-        <Alert
-          status='warning'
-          variant='subtle'
-          flexDirection='column'
-          alignItems='center'
-          justifyContent='center'
-          textAlign='center'
-          height='200px'
-        >
-          <AlertIcon boxSize='40px' mr={0} />
-          <AlertTitle mt={4} mb={1} fontSize='lg'>
-            Microphone Permissions
-          </AlertTitle>
-          <AlertDescription maxWidth='sm'>
-            You have not yet authorized access to the microphone. Please allow permissions to
-            continue.
-          </AlertDescription>
-        </Alert>
-      </Flex>
+      <Center h={'100vh'}>
+        <Spinner thickness='4px' emptyColor='gray.100' color='gray.500' size='xl' />
+      </Center>
     )
   }
   return !permission ? (
