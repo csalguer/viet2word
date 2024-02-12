9bfa5333f312b6d1365e063821929797aae5c5d1
Christopher Salguero <csalguer@alumni.stanford.edu>
2024-02-12 05:55:05 -0800

    Checkpoint WaveSection restructure with content and menubar props

--- features/common/components/WaveSection/index.tsx
+++ features/common/components/WaveSection/index.tsx
@@ -2,6 +2,7 @@ import { ReactElement, useCallback, useLayoutEffect } from 'react'
 import { Text, Flex, Center } from '@chakra-ui/react'
 import styles from '@/features/common/components/WaveSection/wave.module.css'
 import PageHeader from '../PageHeader'
+import { nanoid } from 'nanoid'
 
 // styles
 // const anochecer = ['rgba(19, 4, 64, 1)', 'rgba(99, 43, 60, 1)', 'rgba(97, 86, 130, 1)', 'rgba(135, 120, 126, 1)']
@@ -9,6 +10,75 @@ import PageHeader from '../PageHeader'
 // const alba = []
 // const madrugada = ['rgba(220,250,240,1)', 'rgba(200,224,180,1)', ' rgba(176,225,210,1)', ' rgba(140,215,150,1)']
 // const
+
+interface WaveProps {
+  content?: ReactElement
+}
+// type MenuItem =
+
+const menuItems = [
+  <>
+    <Text key={nanoid(6)}>About</Text>,
+  </>,
+  <>
+    <Text key={nanoid(6)}>Features</Text>,
+  </>,
+  <>
+    <Text key={nanoid(6)}>Demo</Text>,
+  </>,
+  <>
+    <Text key={nanoid(6)}>Documentation</Text>,
+  </>,
+  <>
+    <Text key={nanoid(6)}>Contact</Text>,
+  </>,
+]
+
+const MenuBar = () => {
+  return (
+    <Center width={'100%'} height={'100%'} gap={'0.8em'}>
+      {menuItems.map((item) => item)}
+    </Center>
+  )
+}
+const WaveElement = (): ReactElement => {
+  return (
+    <>
+      <svg
+        className={styles.waves}
+        xmlns='http://www.w3.org/2000/svg'
+        viewBox='0 24 150 28'
+        preserveAspectRatio='none'
+        shapeRendering='auto'
+      >
+        <defs>
+          <path id='gentle-wave' d='M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z' />
+        </defs>
+        <g className={styles.motion}>
+          <use xlinkHref='#gentle-wave' x='48' y='0' fill='rgba(255,255,255,0.7)' />
+          <use xlinkHref='#gentle-wave' x='48' y='3' fill='rgba(255,255,255,0.5)' />
+          <use xlinkHref='#gentle-wave' x='48' y='5' fill='rgba(255,255,255,0.3)' />
+          <use xlinkHref='#gentle-wave' x='48' y='7' fill='#fff' />
+        </g>
+      </svg>
+    </>
+  )
+}
+
+const Wave = ({ content }: WaveProps) => {
+  return (
+    <>
+      <div>
+        <WaveElement
+          animation={() => {
+            return
+          }}
+        />
+        {!!content && content.map((item) => item)}
+      </div>
+    </>
+  )
+}
 
 export const WaveSection = (): ReactElement => {
   const setHoverStyles = useCallback(() => {
@@ -51,26 +121,7 @@ export const WaveSection = (): ReactElement => {
           'linear-gradient(48deg, rgba(255,181,86,1) 0%, rgba(236,105,40,1) 31%, rgba(199,58,103,1) 61%, rgba(149,41,171,1) 100%);'
         }
       >
-        <>
-          <svg
-            className={styles.waves}
-            xmlns='http://www.w3.org/2000/svg'
-            viewBox='0 24 150 28'
-            preserveAspectRatio='none'
-            shapeRendering='auto'
-          >
-            <defs>
-              <path id='gentle-wave' d='M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z' />
-            </defs>
-            <g className={styles.motion}>
-              <use xlinkHref='#gentle-wave' x='48' y='0' fill='rgba(255,255,255,0.7)' />
-              <use xlinkHref='#gentle-wave' x='48' y='3' fill='rgba(255,255,255,0.5)' />
-              <use xlinkHref='#gentle-wave' x='48' y='5' fill='rgba(255,255,255,0.3)' />
-              <use xlinkHref='#gentle-wave' x='48' y='7' fill='#fff' />
-            </g>
-          </svg>
-        </>
-
+        <Wave></Wave>
         <Center position={'absolute'} marginBottom={'200px'} flexDirection={'column'}>
           <Text
             id={'main-action-tag'}
